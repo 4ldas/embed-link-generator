99c23c0107aed39ac0c630ec622f354b7a6840be
Fuyang Liu <fuyangl@spotify.com>
2020-10-27 16:00:06 +0100

    Send config to handler

--- src/routes/embeds.rs
+++ src/routes/embeds.rs
@@ -1,21 +1,25 @@
 use crate::handlers::{errors::handle_rejection, embeds as handlers};
 use crate::models::embeds as models;
+use crate::models::config::Config;
 use warp::Filter;
 
-pub fn embeds() -> impl Filter<Extract = impl warp::Reply, Error = std::convert::Infallible> + Clone {
-    oembed().or(embed()).recover(handle_rejection)
+use std::sync::Arc;
+
+pub fn embeds(config: Arc<Config>) -> impl Filter<Extract=impl warp::Reply, Error=std::convert::Infallible> + Clone {
+    oembed().or(embed(config)).recover(handle_rejection)
 }
 
-pub fn oembed() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+pub fn oembed() -> impl Filter<Extract=impl warp::Reply, Error=warp::Rejection> + Clone {
     warp::path!("oembed")
         .and(warp::get())
         .and(warp::query::<models::Oembed>())
         .and_then(handlers::oembed)
 }
 
-pub fn embed() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+pub fn embed(config: Arc<Config>) -> impl Filter<Extract=impl warp::Reply, Error=warp::Rejection> + Clone {
     warp::path!("embed")
         .and(warp::get())
         .and(warp::query::<models::Embed>())
+        .and(warp::any().map(move || config.clone()))
         .and_then(handlers::create)
 }
