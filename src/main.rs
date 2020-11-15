98fba8de1722256f472f5fb66ab4d6966b980a9f
faith <aldas@protonmail.com>
2020-11-15 15:02:18 +0000

    Added endpoints testing, oembed test added

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod models;
 mod routes;
 mod handlers;
+mod tests;
 
 use std::sync::Arc;
 
---
+++ src/tests/routes.rs
@@ -0,0 +1,22 @@
+use warp::{Filter, http::StatusCode, test::request};
+
+#[tokio::test]
+async fn oembed() {
+   let data = crate::models::embeds::Oembed {
+        etype: None,
+        author_name: Some(String::from("author")),
+        author_url: Some(String::from("https://example.org")),
+        provider_name: Some(String::from("provider")),
+        provider_url: Some(String::from("https://example.org"))
+    };
+    let api = crate::routes::embeds::oembed();
+
+    assert_eq!(
+        request()
+            .method("GET")
+            .path(&format!("/oembed?{}", serde_urlencoded::to_string(&data).unwrap()))
+            .reply(&api)
+            .await.status(),
+            StatusCode::OK
+    );
+}
