d7b11900b141f745efbc616532d831b41618d436
Aldas <40773946+4ldas@users.noreply.github.com>
2022-01-16 11:25:07 +0000

    Update embeds.rs
    Reverted readability change

--- src/handlers/embeds.rs
+++ src/handlers/embeds.rs
@@ -65,9 +65,7 @@ pub async fn create(params: embeds::Embed, config: Arc<Config>) -> Result<impl w
             }
         } else { String::new() },
 
-        format!(r#"<link type="application/json+oembed" href="{}/oembed?{}">"#,
-        config.server.root_url,
-        ammonia::clean(&serde_urlencoded::to_string(embeds::Embed {
+        format!(r#"<link type="application/json+oembed" href="{}/oembed?{}">"#, config.server.root_url, ammonia::clean(&serde_urlencoded::to_string(embeds::Embed {
             title: None,
             description: None,
             site_name: None,
